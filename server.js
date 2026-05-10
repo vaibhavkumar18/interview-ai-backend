3966b063a153e8576b3162dde8ab5116de32bf34
Vaibhav <vaibhavkumarnigam81@gmail.com>
2026-05-10 20:34:16 +0530

    Add PORT

--- server.js
+++ server.js
@@ -6,9 +6,9 @@ const connectDB = require('./src/config/database');
 
 
 connectDB();
+const PORT = process.env.PORT || 3000;
 
 
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
