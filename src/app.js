d87f858f5c6e47ba323f8f9826c652a97c3730a8
Vaibhav <vaibhavkumarnigam30@gmail.com>
2026-05-11 21:11:09 +0530

    Solve the CORS issue

--- src/app.js
+++ src/app.js
@@ -16,6 +16,7 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
+app.options('*', cors());
 console.log("Frontend URL:", config.FRONTEND_URL);
 
 /* require all the routes here */
