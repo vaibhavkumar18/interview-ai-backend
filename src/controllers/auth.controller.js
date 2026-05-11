044beeb1bdb4e8c75e51d0652248f2a1a80d010d
Vaibhav <vaibhavkumarnigam30@gmail.com>
2026-05-11 21:13:38 +0530

    Resolve cookie issue

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -41,7 +41,12 @@ async function registerUserController(req, res) {
     expiresIn: "1d",
   });
 
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
   res.status(201).json({
     message: "User registered successfully",
     user: {
@@ -84,7 +89,12 @@ async function loginUserController(req, res) {
     expiresIn: "1d",
   });
   console.log("token", token);
-  res.cookie("token", token);
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: true,
+    sameSite: "none",
+    maxAge: 24 * 60 * 60 * 1000,
+  });
   res.status(200).json({
     message: "User logged in successfully",
     user: {
