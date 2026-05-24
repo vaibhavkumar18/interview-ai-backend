a427fc7a331cd8ab5265b8d3390863b215f84da8
Vaibhav <vaibhavkumarnigam30@gmail.com>
2026-05-24 20:26:30 +0530

    Added Cached of images for frontend

--- src/app.js
+++ src/app.js
@@ -9,13 +9,25 @@ app.set("trust proxy", 1);
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cookieParser());
-app.use("/images", express.static(path.join(__dirname, "..", "public", "Images")));
 app.use(
   cors({
     origin: config.FRONTEND_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+app.use(
+  "/images",
+  express.static(path.join(__dirname, "..", "public", "Images"), {
+    maxAge: "30d",
+    etag: true,
+    immutable: true,
+    setHeaders: (res) => {
+      res.setHeader("Cache-Control", "public, max-age=2592000, immutable");
+      res.setHeader("Access-Control-Allow-Origin", config.FRONTEND_URL);
+      res.setHeader("Vary", "Origin");
+    },
   }),
 );
 
